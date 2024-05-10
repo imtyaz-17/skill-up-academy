6910741fba0c560bbe4a5c448e258b210c89131f
imtyaz-17 <imtyazit17017@gmail.com>
2024-05-11 01:29:03 +0600

    react bootstrap, route, icons, firebase - setup done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDDM1iHY6OQw_O152fvsrA6_qzjz7t2K-k",
+    authDomain: "skill-up-academy-17.firebaseapp.com",
+    projectId: "skill-up-academy-17",
+    storageBucket: "skill-up-academy-17.appspot.com",
+    messagingSenderId: "878087939370",
+    appId: "1:878087939370:web:3773b88992477c13b52a53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
