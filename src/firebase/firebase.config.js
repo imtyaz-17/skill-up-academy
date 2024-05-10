9b808fec52226469299bd6850c253d0bd29461fa
imtyaz-17 <imtyazit17017@gmail.com>
2024-05-11 01:33:58 +0600

    add Environment Variables for firebase key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDDM1iHY6OQw_O152fvsrA6_qzjz7t2K-k",
-    authDomain: "skill-up-academy-17.firebaseapp.com",
-    projectId: "skill-up-academy-17",
-    storageBucket: "skill-up-academy-17.appspot.com",
-    messagingSenderId: "878087939370",
-    appId: "1:878087939370:web:3773b88992477c13b52a53"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appIdm,
 };
 
 // Initialize Firebase
